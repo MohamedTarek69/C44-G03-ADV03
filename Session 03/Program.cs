b8015dde9c665158f5534d30de54c3f946d984bb
MohamedTarek69 <mohamedyoyo960@gmail.com>
2025-08-29 17:50:23 +0300

    Vedio 01

--- Session 03/Program.cs
+++ Session 03/Program.cs
@@ -2,9 +2,41 @@
 {
     internal class Program
     {
+        public delegate int StringFuncDelegate(string s);
+        // New Delegate [Class]
+        // Ref From Delegate can Refer to Function or More [pointer to Function]
+        // this function can be static Function [Class member function]
+        // this function can be non-static Function [Object member function]
+        // this function must be wtih same signature of delegate
+        // Regardless Function Naming [Method name - Paramter Namr]
+        // Regardless Access Modfire
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //--------------------------------- Vedio 01 ---------------------------------
+            #region Example 01
+            ////int x = StringFunctions.GetCountOfUpperCaseChar(); // invalid
+            //// 1. Declare Reference From Delegate
+            //StringFuncDelegate stringFunc;
+
+            //// 2. Initialize Reference From Delegate[Pointer To Function]
+            //stringFunc = new StringFuncDelegate(StringFunctions.GetCountOfUpperCaseChar);
+
+            //stringFunc = StringFunctions.GetCountOfUpperCaseChar;
+            //stringFunc = StringFunctions.GetCountOfLowerCaseChar;
+
+            ////stringFunc = StringFunctions.GetCountOfUpperCaseChar;
+            ////stringFunc += StringFunctions.GetCountOfLowerCaseChar;
+            ////stringFunc -= StringFunctions.GetCountOfLowerCaseChar;
+            ////stringFunc -= StringFunctions.GetCountOfUpperCaseChar;
+
+            ////3.Use Delegate Reference[Call Method]
+            //int Result = stringFunc.Invoke("RouTe");
+            //Result = stringFunc("MoHaMeD TaReK");
+            //Console.WriteLine(Result);
+
+            #endregion
+
+
         }
     }
 }
---
+++ Session 03/StringFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_03
+{
+    internal static class StringFunctions
+    {
+        public static int GetCountOfUpperCaseChar(string str)
+        {
+            int count = 0;
+            if (str?.Length > 0)
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsUpper(str[i]))
+                        count++;
+            return count;
+        }
+
+        public static int GetCountOfLowerCaseChar(string str)
+        {
+            int count = 0;
+            if (str?.Length > 0)
+                for (int i = 0; i < str.Length; i++)
+                    if (char.IsLower(str[i]))
+                        count++;
+            return count;
+        }
+    }
+}
